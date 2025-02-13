130a8543cf56bac31cd3dcc96d7f5af83e622a18
Wojchik <wojteklipski7@gmail.com>
2025-02-13 11:42:05 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-const menu = document.getElementById("navLines");
+const menu = document.getElementById("navBtn");
 const navigate = document.getElementById("navBar");
+const sect = document.getElementsByClassName('sections')
+
+const phoneRes = window.matchMedia("(max-width: 600px)")
+
+
+matchMedia("(max-width: 600px)")
 
 menu.onclick = function(){
-    navbar.style.display = "none";
-}
+    if(navigate.style.display == 'none'){
+        navigate.style.display = "block";
+    }
+    
+    else {navigate.style.display = "none"
+    }
+    }
+    
